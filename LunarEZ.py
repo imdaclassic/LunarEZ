5e5dccd074cbd2c3d4671198140c3ab3aedd18b6
ImDaClassic <102368409+imdaclassic@users.noreply.github.com>
2025-11-26 07:54:57 +0200

    Update LunarEZ.py

--- LunarEZ.py
+++ LunarEZ.py
@@ -1,8 +1,9 @@
-ver = "0.1"
+ver = "0.0"
 import requests
 import tkinter as tk
 from tkinter import messagebox
 import os
+import sys
 
 def get_latest_string():
     url = "https://raw.githubusercontent.com/imdaclassic/LunarEZ/refs/heads/main/latest.txt"
@@ -19,11 +20,27 @@ def clr():
     else:
         os.system('clear')
 
+def self_update():
+    print("Downloading...")
+    url = "https://raw.githubusercontent.com/imdaclassic/LunarEZ/refs/heads/main/LunarEZ.py"
+    response = requests.get(url)
+    response.raise_for_status()
+    new_code = response.text
+
+    print("Writing...")
+    current_file = os.path.abspath(sys.argv[0])
+
+    with open(current_file, "w", encoding="utf-8") as f:
+        f.write(new_code)
+
+    print(f"{current_file} has been updated.")
+
 if latestver != ver:
     print("New update detected!")
     res = messagebox.askyesno("Update Alert", f"There is a new LunarEZ update (V{latestver}). Do you want to download new update automatically?")
     if res:
-        input("Downloading...")
+        self_update()
+        
 print("No new updates detected.")
 
 clr()
